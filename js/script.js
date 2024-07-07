d5fcb62a87ab99427cf9117eb857d50dfb748426
Akitk0 <akitk000@gmail.com>
2024-07-07 13:04:27 +0300

    добавив текст

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
-let card 
+
 async function getProducts() {
     // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
-    let response = await fetch("store_db.json")
+    let response = await fetch("db.json")
     // Очікуємо на отримання та розпакування JSON-даних з відповіді
     let products = await response.json()
     // Повертаємо отримані продукти
     return products
-        
-    function getCardHtml(item){
+};
+function getCardHTML(item){
+    return `div class="my-card" style="width: 18rem;>
+    <img src="img/${item.image}">
+    <h5 class="text-my-card">{item.title}</h5>
+    <p class="description-card"></p>
+    ${item.price}
+    </p>
+    <button type="bitton" class=" btn-secondary "
+                    data-product='${JSON.stringify(item)}
+    додати в кошик</button>
+    </div>
 
+
+}
+// Викликаємо асинхронну функцію та очікуємо на отримання продуктів
+getProducts().then(function (products) {
+    let productsList = document.querySelector('.products-list')
+    if (productsList) {
+        products.forEach(function (product) {
+            // Відображаємо товари на сторінці
+            productsList.innerHTML += getCardHTML(product)
+        })
     }
+    // Отримуємо всі кнопки "Купити" на сторінці
+    let buyButtons = document.querySelectorAll('.products-list .cart-btn');
+    // Навішуємо обробник подій на кожну кнопку "Купити"
+     if (buyButtons) {
+     buyButtons.forEach(function (button) {
+     button.addEventListener('click', addToCart)
+     });
+     }
+})
 
-    getProducts().then(function(products){
-        products.forEach(function(product)
-            card_list.innerHTML += getCardHtml(product)
-
-    }
-    
-    
 let team1 = document.querySelector('.team1')
 let team2 = document.querySelector('.team2')
 let team3 = document.querySelector('.team3')
@@ -26,6 +48,6 @@ team1.style.height = `${height}px`
 team2.style.height = `${height}px`
 team3.style.height = `${height}px`
 function scrollDown() {
-    window.scrollTo({top: window.innerHeight, behavior: 'smooth' });
+    window.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
 }
 let button = document.querySelector('.arrow-next')
